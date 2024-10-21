df1820dfe235316f26461698b65e4c778817ca91
ShreyaSanjayAkole <shreyaakole143@gmail.com>
2024-10-21 13:59:59 +0530

    Calculate daily wage of employee

--- Employee_wage.js
+++ Employee_wage.js
@@ -1,7 +1,17 @@
-let attendance = Math.floor(Math.random() * 2);
+function check_attendance(){
+    let attendance = Math.floor(Math.random() * 2);
 
-if(attendance === 1){
-    console.log("Employee is present");
-}else{
-    console.log("Employee is absent");
+    if(attendance === 1){
+        return "Employee is present";
+    }else{
+        return "Employee is absent";
+    }
 }
+
+let wage = (wage_per_hour, full_day_hours) => {
+    return wage_per_hour * full_day_hours;
+}
+
+
+console.log(check_attendance());
+console.log(`Daily wage: ${wage(20,8)}`)
