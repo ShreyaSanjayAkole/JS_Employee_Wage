f89b5f1c85f5cb3cc7a29c786a228f41e3ac44e7
ShreyaSanjayAkole <shreyaakole143@gmail.com>
2024-10-21 14:55:30 +0530

    Calculating Wages for a Month

--- Employee_wage.js
+++ Employee_wage.js
@@ -1,6 +1,7 @@
 const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
+const WORKING_DAYS_PER_MONTH = 20 
 
 function check_attendance(){
     let attendance = Math.floor(Math.random() * 3);
@@ -18,23 +19,39 @@ let wage = (working_hour) => {
     return WAGE_PER_HOUR * working_hour;
 }
 
-employee_status = check_attendance();
- 
-switch(employee_status){
-    case "Full_time": 
-        daily_wage = wage(FULL_TIME_HOURS);
-        console.log(`The employee is ${employee_status}.`);
-        console.log(`Daily Wage: ${daily_wage}`);
-        break;
+let calculate_monthly_wage = (daily_wages) => {
     
-    case "Part_time":
-        daily_wage = wage(PART_TIME_HOURS);
-        console.log(`The employee is ${employee_status}.`);
-        console.log(`Daily Wage: ${daily_wage}`);
-        break;
-    
-    default: 
-        console.log(`The employee is ${employee_status}.`);
-        break
-    
+    total_wage = 0
+    for(daily_wage of daily_wages){
+        total_wage += daily_wage
+    }
+    return total_wage
 }
+
+daily_wages = []
+count =0
+for(let day=0 ; day<WORKING_DAYS_PER_MONTH ; day++)
+{
+    employee_status = check_attendance();
+ 
+    switch(employee_status){
+        case "Full_time": 
+            daily_wage = wage(FULL_TIME_HOURS);
+            daily_wages.push(daily_wage);
+            break;
+        
+        case "Part_time":
+            daily_wage = wage(PART_TIME_HOURS);
+            daily_wages.push(daily_wage);
+            break;
+        
+        default: 
+            count +=1;
+            break;             
+    }
+    wage_per_month = calculate_monthly_wage(daily_wages)
+}
+console.log(daily_wages);
+console.log(`Employee is absent for ${count}`)
+temp = 20 - count
+console.log(`Employee wage for ${temp} working days is : ${wage_per_month}`)
