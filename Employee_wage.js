fbe49c55f1cc23ae493e9f9338d078d0a63ad94f
ShreyaSanjayAkole <shreyaakole143@gmail.com>
2024-10-21 15:15:20 +0530

    Calculate Wages till a condition of total working hours or days is reached fora month

--- Employee_wage.js
+++ Employee_wage.js
@@ -2,6 +2,7 @@ const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 const WORKING_DAYS_PER_MONTH = 20 
+const MAX_WORKING_HOURS = 100
 
 function check_attendance(){
     let attendance = Math.floor(Math.random() * 3);
@@ -28,9 +29,12 @@ let calculate_monthly_wage = (daily_wages) => {
     return total_wage
 }
 
-daily_wages = []
-count =0
-for(let day=0 ; day<WORKING_DAYS_PER_MONTH ; day++)
+let total_working_hours = 0
+let daily_wages = []
+let count =0
+let total_working_days = 0
+
+while(total_working_hours < MAX_WORKING_HOURS && total_working_days < WORKING_DAYS_PER_MONTH)
 {
     employee_status = check_attendance();
  
@@ -38,20 +42,26 @@ for(let day=0 ; day<WORKING_DAYS_PER_MONTH ; day++)
         case "Full_time": 
             daily_wage = wage(FULL_TIME_HOURS);
             daily_wages.push(daily_wage);
+            total_working_hours += FULL_TIME_HOURS
+            total_working_days +=1;
             break;
         
         case "Part_time":
             daily_wage = wage(PART_TIME_HOURS);
             daily_wages.push(daily_wage);
+            total_working_hours += PART_TIME_HOURS
+            total_working_days =+1;
             break;
         
         default: 
             count +=1;
+            total_working_days +=1;
             break;             
     }
     wage_per_month = calculate_monthly_wage(daily_wages)
 }
-console.log(daily_wages);
-console.log(`Employee is absent for ${count}`)
-temp = 20 - count
-console.log(`Employee wage for ${temp} working days is : ${wage_per_month}`)
+console.log(`\nDaily wages: ${daily_wages}`)
+console.log(`Employee was absent for ${count} days.`)
+console.log(`Total working days: ${daily_wages.length}`)
+console.log(`Total working hours: ${total_working_hours}`)
+console.log(`Employee's total wage for the month is: ${wage_per_month}`)
